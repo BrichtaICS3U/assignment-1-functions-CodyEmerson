ef1c7a68bcfdbbeca1a4a0e61f8c4f9a78395882
Cody Emery <codykeithemery6@gmail.com>
2018-03-28 10:41:23 -0400

    .

--- main.py
+++ main.py
@@ -13,23 +13,24 @@ def FtoC (y):
     y  = (0.55556)*(y-32)
     return y
 
-print('Convert to Celsius or to Fahrenheit?')
-ans = str(input())
-if ans == str('Fahrenheit') or ans == str('F'):
-    temperature = int(input('Enter your temperature in Celsius: ' ))
-    if temperature <= -273:
-        print('Restart the shell and print a temperature higher than -273C')
-    else:
-        temperature = CtoF(temperature)
-        print(round(temperature))
-        print('Restart the shell to continue')
 
-elif ans == str('Celsius') or ans == str('C'):
-    temperature = int(input('Enter your temperature in Fahrenheit: ' ))
-    if temperature <= -459 :
-        print('Restart the shell and print a temperature higher than -459F')
-    else:
-        temperature = FtoC(temperature)
-        print(round(temperature))
-        print('Restart the shell to continue')
+answ = True
+while answ == True:
+    print('Convert to Celsius or to Fahrenheit?')
+    ans = str(input())
+    if ans == str('Fahrenheit') or ans == str('F'):
+        temperature = int(input('Enter your temperature in Celsius: ' ))
+        if temperature <= -273:
+            print('Print a temperature higher than -273C')
+        else:
+            temperature = CtoF(temperature)
+            print(round(temperature))
+
+    elif ans == str('Celsius') or ans == str('C'):
+        temperature = int(input('Enter your temperature in Fahrenheit: ' ))
+        if temperature <= -459 :
+            print('Print a temperature higher than -459F')
+        else:
+            temperature = FtoC(temperature)
+            print(round(temperature))
     
