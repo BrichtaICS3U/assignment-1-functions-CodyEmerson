d95547c9d9c16531b0589e5a2a85851fa8a8b9c6
Cody Emery <codykeithemery6@gmail.com>
2018-03-28 10:04:59 -0400

    .

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 # Assignment 1
 # ICS3U
-# <your name>
+# <Cody Emery>
 # March 28, 2018
 
 ###### uncomment this when you are ready to work on it
-#def CtoF ():
-#
+def CtoF (x):
+    x = (1.8)*x+32
+    return x
 
 ###### uncomment this when you are ready to work on it
 #def FtoC ():
 #
 
-temperature = int(input('Enter your temperature in Celsius: '))
-print(temperature)
+temperature = int(input('Enter your temperature in Celsius: ' ))
+print(CtoF(temperature))
